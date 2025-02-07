9959a49b8c1fe0021fcae13d8705d5505b52ada0
derRaab <github.com@derRaab.com>
2025-02-07 13:26:03 +0100

    Update parse.ts

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,5 @@
-import { createPathSegmentFromArrayIndex, createPathSegmentFromObjectKey } from "./path";
-import { parseReferencePath } from "./reference";
+import { createPathSegmentFromArrayIndex, createPathSegmentFromObjectKey, joinPath } from "./path";
+import { parseReferencePath, stringifyReferencePath } from "./reference";
 
 
 const collectObjectPaths = (parentObj: any, objPathMap: Map<Object,string>, path: string[]) => {
@@ -7,7 +7,7 @@ const collectObjectPaths = (parentObj: any, objPathMap: Map<Object,string>, path
         return;
     }
     
-    objPathMap.set(parentObj, path.join(""));
+    objPathMap.set(parentObj, joinPath(path));
 
     if (Array.isArray(parentObj)) {
         for (let i = 0; i < parentObj.length; i++) {
@@ -21,14 +21,23 @@ const collectObjectPaths = (parentObj: any, objPathMap: Map<Object,string>, path
     }
 }
 
-const parseRecursion = (parentObj: any, objPathMap: Map<any,string>, pathObjectMap: Map<string, any>) => {
+const parseRecursion = (parentObj: any, objReferenceMap: Map<Object,string>, pathObjectMap: Map<string, any>, path: string[], unresolvedReferences: string[]) => {
     if (typeof parentObj !== "object") {
         return;
     }
 
+    let reference = stringifyReferencePath(joinPath(path));
+    objReferenceMap.set(parentObj, reference);
+
     if (Array.isArray(parentObj)) {
         for (let i = 0; i < parentObj.length; i++) {
-            const childObject: any = parentObj[i];
+            const childObject = parentObj[i];
+
+            if (objReferenceMap.has(childObject)) {
+                parentObj[i] = objReferenceMap.get(childObject);
+                continue;
+            }
+
             if (typeof childObject === "string") {
                 const path = parseReferencePath(childObject);
                 if (path !== null) {
@@ -37,18 +46,24 @@ const parseRecursion = (parentObj: any, objPathMap: Map<any,string>, pathObjectM
                         parentObj[i] = referenceObject;
                     }
                     else {
-                        throw new Error(childObject + " not resolved");
+                        unresolvedReferences.push(childObject);
                     }
                 }
                 continue;
             }
-            parseRecursion(childObject, objPathMap, pathObjectMap);
+            parseRecursion(childObject, objReferenceMap, pathObjectMap, path.concat([createPathSegmentFromArrayIndex(i)]), unresolvedReferences);
         }
         return;
     }
 
     for (const key in parentObj) {
-        const childObject: any = parentObj[key];
+        const childObject = parentObj[key];
+
+        if (objReferenceMap.has(childObject)) {
+            parentObj[key] = objReferenceMap.get(childObject);
+            continue
+        }
+
         if (typeof childObject === "string") {
             const path = parseReferencePath(childObject);
             if (path !== null) {
@@ -57,23 +72,31 @@ const parseRecursion = (parentObj: any, objPathMap: Map<any,string>, pathObjectM
                     parentObj[key] = referenceObject;
                 }
                 else {
-                    throw new Error(childObject + " not resolved");
+                    unresolvedReferences.push(childObject);
                 }
             }
             continue;
         }
-        parseRecursion(childObject, objPathMap, pathObjectMap);
+        parseRecursion(childObject, objReferenceMap, pathObjectMap, path.concat([createPathSegmentFromObjectKey(key)]), unresolvedReferences);
     }
 }
 
 export const parseCircularReferences = (data: Object) => {
-    const objPathMap = new Map<Object, string>();
-    collectObjectPaths(data, objPathMap, []);
+    // Sometimes referenced objects are not yet parsed. We need to parse the data multiple times until all references are resolved.
+    let unresolvedReferencesLength = -1;
+    let unresolvedReferences: string[] = [];
+    while (unresolvedReferencesLength !== unresolvedReferences.length) {
+        unresolvedReferencesLength = unresolvedReferences.length;
+        unresolvedReferences = [];
 
-    const pathObjectMap = new Map<string, Object>();
-    for (const [object, path] of objPathMap) {
-        pathObjectMap.set(path, object);
+        // Collect all existing object paths
+        const objPathMap = new Map<Object, string>();
+        collectObjectPaths(data, objPathMap, []);
+        const pathObjectMap = new Map<string, Object>();
+        for (const [object, path] of objPathMap) {
+            pathObjectMap.set(path, object);
+        }
+
+        parseRecursion(data, new Map<Object, string>(), pathObjectMap, [], unresolvedReferences);
     }
-
-    parseRecursion(data, new Map<any, string>(), pathObjectMap);
 }
