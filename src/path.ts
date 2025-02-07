1ad19c169c8d273ca4232c27b8d389af21a69a3e
derRaab <github.com@derRaab.com>
2025-02-07 13:22:03 +0100

    Update path.ts

--- src/path.ts
+++ src/path.ts
@@ -3,5 +3,37 @@ export const createPathSegmentFromArrayIndex = (index: number) => {
 }
 
 export const createPathSegmentFromObjectKey = (key: string) => {
-    return "." + key;
+    return '["' + key + '"]';
+}
+
+export const joinPath = (pathSegments: string[]) => {
+    return pathSegments.join("");
+}
+
+const trimString = (string: string, char: string) => {
+    while (string.startsWith(char)) {
+        string = string.substring(1);
+    }
+    while (string.endsWith(char)) {
+        string = string.substring(0, string.length - 1);
+    }
+    return string;
+}
+
+export const splitPath = (path: string) => {
+    if (path === "") {
+        return [];
+    }
+    const pathSegmentStrings = path.split( "][");
+    return pathSegmentStrings.map((segmentString, index) => {
+        segmentString = trimString(segmentString, "[");
+        segmentString = trimString(segmentString, "]");
+        
+        const isKey = segmentString.startsWith('"');
+        if (isKey) {
+            return trimString(segmentString, '"');
+        }
+
+        return parseInt(segmentString);
+    });
 }
